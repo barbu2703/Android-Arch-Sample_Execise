bb481a75e804487a2f1fad32577f09682c1923f3
Adrian Barbu <barbu2703@outlook.com>
2021-06-15 10:00:52 +0200

    [2021.6.14][Adrian] Commit updated code for Execise 6
    - Implement Execise 6
    Allow the user to search for tasks in the list screen.

--- app/src/main/java/com/majestykapps/arch/presentation/tasks/TasksFragment.kt
+++ app/src/main/java/com/majestykapps/arch/presentation/tasks/TasksFragment.kt
@@ -1,8 +1,13 @@
 package com.majestykapps.arch.presentation.tasks
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -14,12 +19,20 @@ import com.majestykapps.arch.DetailActivity
 import com.majestykapps.arch.R
 import com.majestykapps.arch.domain.entity.Task
 import com.majestykapps.arch.presentation.adapter.TaskListAdapter
-import kotlinx.android.synthetic.main.fragment_tasks.swipeRefresh
-import kotlinx.android.synthetic.main.fragment_tasks.list_recycler_view
+import com.yalantis.jellytoolbar.listener.JellyListener
+import kotlinx.android.synthetic.main.fragment_tasks.*
 
+/**
+ * @description     Tasks List Fragment
+ *
+ * @author          Adrian
+ */
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by activityViewModels()
+    private var editText: AppCompatEditText? = null
+
+    private var allTasks: List<Task>? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,6 +52,29 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         ).apply {
             list_recycler_view.addItemDecoration(this)
         }
+
+        // Tool bar
+        editText = LayoutInflater.from(activity).inflate(R.layout.edit_text, null) as AppCompatEditText
+        editText!!.setBackgroundResource(android.R.color.transparent)
+        editText!!.maxLines = 1
+        editText!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val key = editText!!.text.toString()
+                if (key.isEmpty()) {
+                    updateTaskList(allTasks)
+                } else {
+                    val filteredTask = allTasks!!.filter { it.title.contains(key, true) || it.description.contains(key, true) }
+                    updateTaskList(filteredTask)
+                }
+            }
+        })
+
+        toolbar.contentView = editText
+
+        toolbar.jellyListener = jellyListener
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -63,15 +99,40 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
             tasks.observe(viewLifecycleOwner, Observer { tasks ->
                 Log.d(TAG, "tasks observed: $tasks")
 
-                list_recycler_view.apply {
-                    layoutManager = LinearLayoutManager(activity)
-                    adapter = TaskListAdapter(tasks) { task: Task ->
-                        activity?.let {
-                            DetailActivity.navigateTODetailActivity(it, task)
-                        }
+                allTasks = tasks
+                updateTaskList(tasks)
+            })
+        }
+    }
+
+    private fun updateTaskList(tasks: List<Task>?) {
+        list_recycler_view.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = tasks?.let {
+                TaskListAdapter(it) { task: Task ->
+                    activity?.let {
+                        DetailActivity.navigateTODetailActivity(it, task)
                     }
                 }
-            })
+            }
+        }
+    }
+
+    private val jellyListener: JellyListener = object : JellyListener() {
+        override fun onToolbarExpandingStarted() {
+            toolbar.toolbar!!.title = ""
+        }
+
+        override fun onToolbarCollapsingStarted() {
+            toolbar.toolbar!!.title = getString(R.string.app_name)
+        }
+
+        override fun onCancelIconClicked() {
+            if (TextUtils.isEmpty(editText!!.text)) {
+                toolbar.collapse()
+            } else {
+                editText!!.text!!.clear()
+            }
         }
     }
 
